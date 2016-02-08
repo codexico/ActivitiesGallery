2d54dc89b213b33bd5d59eff57b634174903b841
Francisco Kahil <codexico@gmail.com>
2016-02-08 19:16:54 -0200

    Android Studio Analyse suggestions

--- app/src/main/java/com/example/fk/activitiesgallery/EmptyActivity.java
+++ app/src/main/java/com/example/fk/activitiesgallery/EmptyActivity.java
@@ -1,9 +1,8 @@
 package com.example.fk.activitiesgallery;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class EmptyActivity extends AppCompatActivity {
+public class EmptyActivity extends NavigationDrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
