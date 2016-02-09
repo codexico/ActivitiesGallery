19c518654e268f66a4863dab79e585053b3194ea
Francisco Kahil <codexico@gmail.com>
2016-02-08 22:29:55 -0200

    Add navigation drawer to scrolling activity

--- app/src/main/java/com/example/fk/activitiesgallery/ScrollingActivity.java
+++ app/src/main/java/com/example/fk/activitiesgallery/ScrollingActivity.java
@@ -2,12 +2,13 @@ package com.example.fk.activitiesgallery;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class ScrollingActivity extends AppCompatActivity {
+public class ScrollingActivity extends NavigationDrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +25,9 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // connect NavigationDrawerActivity actions
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 }
