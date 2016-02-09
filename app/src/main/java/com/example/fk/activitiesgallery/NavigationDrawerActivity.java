de8582399a22c866c132c09505b307ce74807a66
Francisco Kahil <codexico@gmail.com>
2016-02-09 00:55:26 -0200

    Add intent to Settings activity on options menu
    As the other activities are using the same menu, its only necessary
    to add in the notification drawer.

--- app/src/main/java/com/example/fk/activitiesgallery/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/fk/activitiesgallery/NavigationDrawerActivity.java
@@ -70,6 +70,8 @@ public class NavigationDrawerActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
